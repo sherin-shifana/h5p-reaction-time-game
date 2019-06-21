da3985bb30f78241e6ac1da0f1b7a261718a3ee6
sherin-shifana <sherinshifana1997@gmail.com>
2019-06-21 17:46:34 +0530

    commit

--- js/h5p-reaction-time-game.js
+++ js/h5p-reaction-time-game.js
@@ -16,23 +16,57 @@ H5P.ReactionGame = (function ($, UI) {
 
     $('<div class="start">Click here!!</div>').appendTo($mainContainer);
     const $startbutton = $('<div class="button-circle"><i class="fa fa-hand-o-up" aria-hidden="true"></i></div>');
+    // that.$retryButton = UI.createButton({
+    //       title: 'Retry',
+    //       'text': 'Retry',
+    //       'class': 'retry-button',
+    //       click: resetAll()
+    // });
 
     function setColor() {
-
       $mainContainer.empty();
       $mainContainer.css('background-color', "#36f950");
       this.createdTime = Date.now();
+      // this.$timeContainer = $('<div class="reaction-time"></div>').appendTo(this.$panel);
+      this.clkCount = 0;
+
       $mainContainer.click(function(){
         afterClick();
+
       });
       console.log(this.createdTime);
     }
+
+    function resetAll () {
+      $mainContainer.empty();
+    }
+
     function afterClick() {
       const that = this;
-      this.clickedTime = Date.now();
-      this.reactionTime = (this.clickedTime - that.createdTime) / 1000;
-      $('<div>'+this.reactionTime+'</div>').appendTo($mainContainer);
+      that.clkCount += 1;
+      this.$panel = $('<div class="panel"></panel>');
+      this.totalTime = 0;
+      if (that.clkCount <= 5) {
+        this.clickedTime = Date.now();
+        this.reactionTime = (this.clickedTime - that.createdTime) / 1000;
+        this.totalTime += this.reactionTime;
+        this.clickedTime = 0;
+        this.$retrybutton = $('<div class="button-circle"><i class="fa fa-repeat" aria-hidden="true"></i></div>').appendTo($mainContainer);
+        $('<div class="reaction-time">Attempts :'+that.clkCount+'/'+5+' Reaction Time : '+this.reactionTime+'</div>').appendTo(that.$panel);
+
+
+      }
+
+      else if(that.clkCount === 5) {
+        $mainContainer.unbind();
+      }
+      // that.$retryButton
+      this.$panel.appendTo($mainContainer);
+      // setTimeout(function(){
+      // 	// $retryButton.show();
+      // },1000);
     }
+
     function startFn() {
       const that = this;
       $mainContainer.empty();
